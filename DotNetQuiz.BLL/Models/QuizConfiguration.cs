d0eec32bf56196e7c456d16060b7f2ada339d874
Malukov Nikita <nikitaminsk833@gmail.com>
2022-01-22 19:22:06 +0300

    update model, set initial values

--- DotNetQuiz.BLL/Models/QuizConfiguration.cs
+++ DotNetQuiz.BLL/Models/QuizConfiguration.cs
@@ -2,8 +2,8 @@
 {
     public class QuizConfiguration
     {
-        public int RoundDuration { get; set; }
-        public int MaxPlayers { get; set; }
+        public int RoundDuration { get; set; } = 60;
+        public int MaxPlayers { get; set; } = 4;
         public double StreakMultiplier { get; set; }
         public double TimeMultiplier { get; set; }
         public bool AnswerIgnoreCase { get; set; }
--- DotNetQuiz.BLL/Models/QuizQuestionPack.cs
+++ DotNetQuiz.BLL/Models/QuizQuestionPack.cs
@@ -3,7 +3,6 @@
     public class QuizQuestionPack
     {
         public int QuestionPackId { get; set; }
-
         public ICollection<QuizQuestion> Questions { get; set; }
     }
 }
