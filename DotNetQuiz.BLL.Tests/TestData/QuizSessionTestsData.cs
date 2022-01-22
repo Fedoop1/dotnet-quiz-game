012dafe68aea74ccd3f61d1c9e52bace18a6e096
Malukov Nikita <nikitaminsk833@gmail.com>
2022-01-23 02:01:13 +0300

    update test structure, add implicit usings, add round statistic analyzer unit tests

---
+++ DotNetQuiz.BLL.Tests/TestData/QuizSessionTestsData.cs
@@ -0,0 +1,43 @@
+﻿using DotNetQuiz.BLL.Models;
+
+namespace DotNetQuiz.BLL.Tests.TestData;
+
+internal static class QuizSessionTestsData
+{
+    internal static readonly QuizQuestionPack DefaultQuestionPack;
+    internal static readonly QuizConfiguration DefaultQuizConfiguration;
+    internal static readonly QuizPlayer[] QuizPlayers;
+
+    static QuizSessionTestsData()
+    {
+        DefaultQuestionPack = new()
+        {
+            QuestionPackId = 1,
+            Questions = new QuizQuestion[]
+            {
+                new()
+                {
+                    QuestionId = 1,
+                    Answer = new QuestionAnswer()
+                    {
+                        AnswerContent = "rightAnswer"
+                    },
+                    QuestionReward = 100
+                }
+            }
+        };
+
+        DefaultQuizConfiguration = new()
+        {
+            QuestionPack = DefaultQuestionPack,
+        };
+
+        QuizPlayers = new QuizPlayer[] 
+        {
+            new () { PlayerId = 1, PlayerNickName = "nickname1" },
+            new () { PlayerId = 2, PlayerNickName = "nickname2" },
+            new () { PlayerId = 3, PlayerNickName = "nickname3" },
+            new () { PlayerId = 4, PlayerNickName = "nickname4" },
+        };
+    }
+}
---
+++ DotNetQuiz.BLL.Tests/TestData/RoundStatisticAnalyzerTestsData.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using DotNetQuiz.BLL.Models;
+using NUnit.Framework;
+
+namespace DotNetQuiz.BLL.Tests.TestData;
+
+internal class RoundStatisticAnalyzerTestsData
+{
+    public static IEnumerable<TestCaseData> QuizRounds()
+    {
+        yield return new TestCaseData(new QuizRound()
+        {
+            Answers = new QuizPlayerAnswer[]
+            {
+                new()
+                {
+                    AnswerContent = "answer",
+                    AnswerTime = 1,
+                },
+                new()
+                {
+                    AnswerContent = "answer",
+                    AnswerTime = 1,
+                },
+                new()
+                {
+                    AnswerContent = "answer",
+                    AnswerTime = 1,
+                },
+                new()
+                {
+                    AnswerContent = "answer",
+                    AnswerTime = 1,
+                },
+            }
+        }, new RoundStatistic()
+        {
+            AnswerStatistic = new Dictionary<string, int>()
+            {
+                ["answer"] = 4,
+            },
+            AverageAnswerTime = TimeSpan.FromMilliseconds(1),
+        });
+
+        yield return new TestCaseData(new QuizRound()
+        {
+            Answers = new QuizPlayerAnswer[]
+            {
+                new()
+                {
+                    AnswerContent = "firstAnswer",
+                    AnswerTime = 1,
+                },
+                new()
+                {
+                    AnswerContent = "secondAnswer",
+                    AnswerTime = 2,
+                },
+                new()
+                {
+                    AnswerContent = "thirdAnswer",
+                    AnswerTime = 3,
+                },
+                new()
+                {
+                    AnswerContent = "fourthAnswer",
+                    AnswerTime = 4,
+                },
+            }
+        }, new RoundStatistic()
+        {
+            AnswerStatistic = new Dictionary<string, int>()
+            {
+                ["firstAnswer"] = 1,
+                ["secondAnswer"] = 1,
+                ["thirdAnswer"] = 1,
+                ["fourthAnswer"] = 1,
+            },
+            AverageAnswerTime = TimeSpan.FromMilliseconds(2.5),
+        });
+    }
+}
+
