7f3ed0bcb86381f68c7bb79bff2e528425ec20dd
Nikita Malukov <nikita_malukov@epam.com>
2022-02-12 18:08:32 +0300

    update IQuizSessionService API

--- DotNetQuiz.BLL/Interfaces/IQuizSessionService.cs
+++ DotNetQuiz.BLL/Interfaces/IQuizSessionService.cs
@@ -1,15 +1,20 @@
 ﻿using DotNetQuiz.BLL.Models;
+using DotNetQuiz.BLL.Models.enums;
 
 namespace DotNetQuiz.BLL.Interfaces
 {
     public interface IQuizSessionService
     {
+        QuizRound CurrentRound { get; }
+        IReadOnlyCollection<QuizPlayer> SessionPlayers { get; }
+        SessionState SessionState { get; }
+
         void UploadQuizConfiguration(QuizConfiguration configuration);
         void AddPlayerToSession(int playerId, string? playerNickName);
         void RemovePlayerFromSession(int playerId);
         void StartGame();
         void SubmitAnswer(QuizPlayerAnswer answer);
-        RoundStatistic GetRoundStatistic(QuizRound round);
-        QuizRound NextRound();
+        void NextRound();
+        RoundStatistic BuildCurrentRoundStatistic();
     }
 }
