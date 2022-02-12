757bc4ee4d8ffa73d814fbdf660cedaa05912097
Nikita Malukov <nikita_malukov@epam.com>
2022-02-13 00:49:55 +0300

    Add session and exception filters to quiz controller

---
+++ DotNetQuiz.WebApi/Infrastructure/Filters/ExceptionHandlerFilterAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DotNetQuiz.WebApi.Infrastructure.Filters
+{
+    public class ExceptionHandlerFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var environment = context.HttpContext.RequestServices.GetService<IWebHostEnvironment>();
+            var logger = context.HttpContext.RequestServices.GetService<ILogger<ExceptionHandlerFilterAttribute>>();
+
+            string problemDetails =
+                environment.IsDevelopment() ? context.Exception.Message : "Please contact support and let us know to what happened";
+
+            logger.LogError(context.Exception, $"An exception occurred in {context.Exception.TargetSite.Name}");
+
+            context.Result = new BadRequestObjectResult(new ProblemDetails()
+            {
+                Title = "An error occurred",
+                Status = StatusCodes.Status400BadRequest,
+                Detail = problemDetails, 
+                Instance = $"DotNetQuizGame:{context.Exception.HResult}"
+            });
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ DotNetQuiz.WebApi/Infrastructure/Filters/SessionFilterAttribute.cs
@@ -0,0 +1,31 @@
+﻿using DotNetQuiz.WebApi.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DotNetQuiz.WebApi.Infrastructure.Filters
+{
+    public class SessionFilterAttribute : ActionFilterAttribute
+    {
+        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            const string sessionIdActionParamName = "sessionId";
+
+            if(!context.ActionArguments.TryGetValue(sessionIdActionParamName, out var sessionIdObj))
+            {
+                context.Result = new BadRequestObjectResult(new { errorMessage = "sessionId is required!"});
+            }
+
+            var sessionId = (Guid)sessionIdObj!;
+
+            var quizHandlersManager = context.HttpContext.RequestServices.GetService<IQuizHandlersManager>()!;
+
+            if (quizHandlersManager.GetSessionHandler(sessionId) is null)
+            {
+                context.Result = new BadRequestObjectResult(new
+                    { errorMessage = $"Quiz session with id[{sessionId}] doesn't exist" });
+            }
+
+            return next();
+        }
+    }
+}
