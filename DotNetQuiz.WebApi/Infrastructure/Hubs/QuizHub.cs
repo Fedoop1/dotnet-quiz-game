ce115a5e9465aef87635d95b993552bb68d802c5
Malukov Nikita <nikitaminsk833@gmail.com>
2022-02-26 15:20:43 +0300

    Add quiz hub implementation

---
+++ DotNetQuiz.WebApi/Infrastructure/Hubs/QuizHub.cs
@@ -0,0 +1,24 @@
+﻿using DotNetQuiz.BLL.Interfaces;
+using DotNetQuiz.BLL.Models;
+using DotNetQuiz.WebApi.Infrastructure.Interfaces;
+using DotNetQuiz.WebApi.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DotNetQuiz.WebApi.Infrastructure.Hubs
+{
+    public class QuizHub : Hub, IQuizHub
+    {
+        private readonly IQuizSessionHandler sessionHandler;
+        public QuizHub(IQuizSessionHandler sessionHandler) => this.sessionHandler = sessionHandler;
+
+        public async Task SendQuestionAsync(QuizRoundModel quizRound) => await Clients.Users(GetQuizPlayersIds()).SendCoreAsync(
+            "roundQuestion", new[] { quizRound });
+
+        public void ReceiveQuestion(QuizPlayerAnswer answer) => this.sessionHandler.SubmitAnswer(answer);
+
+        public async Task SendRoundStatisticAsync(RoundStatistic roundStatistic) => await Clients.Users(GetQuizPlayersIds())
+            .SendCoreAsync("roundStatistic", new[] { roundStatistic });
+
+        private IEnumerable<string> GetQuizPlayersIds() => this.sessionHandler.SessionPlayers.Select(p => p.Id.ToString());
+    }
+}
