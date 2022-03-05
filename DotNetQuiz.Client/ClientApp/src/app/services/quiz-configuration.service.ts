6c896cc65732c3f0ba0d32e3af52948492914f44
Malukov Nikita <nikitaminsk833@gmail.com>
2022-03-05 19:45:13 +0300

    Add quiz configuration service, setup signalR logic in quiz service

---
+++ DotNetQuiz.Client/ClientApp/src/app/services/quiz-configuration.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { QuestionPack } from '../models/questio-pack.model';
+import { QuizConfiguration } from '../models/quiz-configuration.model';
+
+@Injectable()
+export class QuizConfigurationService {
+  public parseQuestionPack(file: File): Observable<QuestionPack | undefined> {
+    return from(file.text()).pipe(
+      map((fileContent) => {
+        const questionPack = JSON.parse(fileContent);
+        return this.validateQuestionPack(questionPack)
+          ? questionPack
+          : undefined;
+      })
+    );
+  }
+
+  public validateQuizConfiguration(
+    quizConfiguration: QuizConfiguration
+  ): boolean {
+    return (
+      quizConfiguration.roundDuration >= 5 &&
+      quizConfiguration.maxPlayers > 0 &&
+      quizConfiguration.streakMultiplier >= 1 &&
+      quizConfiguration.timeMultiplier >= 1 &&
+      this.validateQuestionPack(quizConfiguration.questionPack!)
+    );
+  }
+
+  private validateQuestionPack(questionPack?: QuestionPack): boolean {
+    return (
+      (questionPack?.questions &&
+        questionPack.questions.every(
+          (question) =>
+            question.questionId &&
+            question.questionReward >= 0 &&
+            question.content &&
+            question.answer
+        )) ||
+      false
+    );
+  }
+}
--- DotNetQuiz.Client/ClientApp/src/app/services/quiz.service.ts
+++ DotNetQuiz.Client/ClientApp/src/app/services/quiz.service.ts
@@ -1,21 +1,69 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { NEVER, Observable, of, throwError } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { NEVER, Observable, of, Subject, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { QuizSession } from '../components/join-session/models/quiz-session.model';
 import AppConfiguration from '../models/constants/configuraion';
 import { QuizConfiguration } from '../models/quiz-configuration.model';
+import { QuizPlayer } from '../models/quiz-player.model';
+import { DestroyableComponent } from '../utils/destroyable-component/destroyable.component';
 
 @Injectable()
-export class QuizService {
-  constructor(private readonly httpClient: HttpClient) {}
+export class QuizService implements OnDestroy {
+  private quizHubConnection!: HubConnection;
+
+  private _playerAdded$: Subject<void> = new Subject();
+  private _playerRemoved$: Subject<void> = new Subject();
+  private _connectionClose$: Subject<string> = new Subject();
+  private _userConnectionClosed$: Subject<number> = new Subject();
+
+  public playerAdded$ = this._playerAdded$.asObservable();
+  public playerRemoved$ = this._playerRemoved$.asObservable();
+  public userConnectionClosed$ = this._userConnectionClosed$.asObservable();
+  public connectionClose$ = this._connectionClose$.asObservable();
+
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly route: ActivatedRoute
+  ) {
+    this.buildConnection();
+    this.setupSubscribers();
+  }
+
+  ngOnDestroy(): void {
+    this.quizHubConnection.stop();
+  }
+
+  public removePlayer(
+    playerId: number,
+    sessionId: string,
+    force: boolean = false
+  ) {
+    return this.httpClient
+      .post(
+        `${AppConfiguration.BackendServerAddress}/${AppConfiguration.QuizControllerAddress}/${sessionId}/RemovePlayer/${playerId}`,
+        null,
+        { params: { force: force } }
+      )
+      .pipe(catchError(this.catchResponseError));
+  }
 
   public createQuizSession(): Observable<string> {
     return this.httpClient
       .post<string>(
         `${AppConfiguration.BackendServerAddress}/${AppConfiguration.QuizControllerAddress}/Create`,
-        undefined,
         undefined
+      )
+      .pipe(catchError(this.catchResponseError));
+  }
+
+  public closeQuizSession(sessionId: number): Observable<void> {
+    return this.httpClient
+      .post<void>(
+        `${AppConfiguration.BackendServerAddress}/${AppConfiguration.QuizControllerAddress}/${sessionId}/Close/`,
+        null
       )
       .pipe(catchError(this.catchResponseError));
   }
@@ -26,9 +74,17 @@ export class QuizService {
   ): Observable<void> {
     return this.httpClient
       .post<void>(
-        `${AppConfiguration.BackendServerAddress}/${AppConfiguration.QuizControllerAddress}/Configure`,
-        { sessionId: sessionId, quizConfiguration: quizConfiguration },
+        `${AppConfiguration.BackendServerAddress}/${AppConfiguration.QuizControllerAddress}/${sessionId}/Configure/`,
+        quizConfiguration,
         undefined
+      )
+      .pipe(catchError(this.catchResponseError));
+  }
+
+  public loadSessionPlayers(sessionId: string): Observable<QuizPlayer[]> {
+    return this.httpClient
+      .get<QuizPlayer[]>(
+        `${AppConfiguration.BackendServerAddress}/${AppConfiguration.QuizControllerAddress}/${sessionId}/GetSessionPlayers/`
       )
       .pipe(catchError(this.catchResponseError));
   }
@@ -47,13 +103,10 @@ export class QuizService {
     sessionId: string
   ): Observable<void> {
     return this.httpClient.post<void>(
-      `${AppConfiguration.BackendServerAddress}/${AppConfiguration.QuizControllerAddress}/AddPlayer`,
+      `${AppConfiguration.BackendServerAddress}/${AppConfiguration.QuizControllerAddress}/${sessionId}/AddPlayer`,
       {
-        sessionId: sessionId,
-        player: {
-          nickName: nickName,
-          sessionId: sessionId,
-        },
+        nickName: nickName,
+        id: userId,
       }
     );
   }
@@ -61,5 +114,36 @@ export class QuizService {
   private catchResponseError(error: HttpErrorResponse) {
     console.error('Backend return error:', error);
     return throwError(() => error);
+  }
+
+  private buildConnection() {
+    this.quizHubConnection = new HubConnectionBuilder()
+      .withUrl(
+        `${AppConfiguration.BackendServerAddress}/quiz/${this.route.snapshot.queryParams?.sessionId}`
+      )
+      .build();
+  }
+
+  private setupSubscribers() {
+    this.quizHubConnection.on('playerAdded', () => {
+      this._playerAdded$.next();
+    });
+
+    this.quizHubConnection.on('playerRemoved', () => {
+      this._playerRemoved$.next();
+    });
+
+    this.quizHubConnection.on(
+      'userConnectionClosed',
+      (user: { userId: number }) => {
+        this._userConnectionClosed$.next(user.userId);
+      }
+    );
+
+    this.quizHubConnection.on('connectionClosed', (message: string[]) => {
+      this._connectionClose$.next(
+        message ? message[0] : 'Connection was closed'
+      );
+    });
   }
 }
