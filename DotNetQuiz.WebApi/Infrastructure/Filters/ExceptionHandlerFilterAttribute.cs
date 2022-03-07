59aa6120b6d7026f8df23ad78457baba2bced3ef
Malukov Nikita <nikitaminsk833@gmail.com>
2022-03-07 23:41:47 +0300

    Refactor exception filter

--- DotNetQuiz.WebApi/Infrastructure/Filters/ExceptionHandlerFilterAttribute.cs
+++ DotNetQuiz.WebApi/Infrastructure/Filters/ExceptionHandlerFilterAttribute.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace DotNetQuiz.WebApi.Infrastructure.Filters
 {
-    public class ExceptionHandlerFilterAttribute : ExceptionFilterAttribute
+    public partial class ExceptionHandlerFilterAttribute : ExceptionFilterAttribute
     {
         public override void OnException(ExceptionContext context)
         {
@@ -13,17 +13,21 @@ namespace DotNetQuiz.WebApi.Infrastructure.Filters
             string problemDetails =
                 environment.IsDevelopment() ? context.Exception.Message : "Please contact support and let us know to what happened";
 
-            logger.LogError(context.Exception, $"An exception occurred in {context.Exception.TargetSite.Name}");
+            logger.LogError(context.Exception, $"An exception occurred in {context.Exception.TargetSite?.Name}");
 
             context.Result = new BadRequestObjectResult(new ProblemDetails()
             {
                 Title = "An error occurred",
                 Status = StatusCodes.Status400BadRequest,
                 Detail = problemDetails, 
-                Instance = $"DotNetQuizGame:{context.Exception.HResult}"
+                Instance = $"DotNetQuizGame:{Guid.NewGuid()}"
             });
 
+            LogException(logger, nameof(context.Exception), context.Exception.Message);
             context.ExceptionHandled = true;
         }
+
+        [LoggerMessage(0, LogLevel.Error, "An {exceptionName} exception occurred. Exception message: {exceptionMessage}")]
+        private static partial void LogException(ILogger logger, string exceptionName, string exceptionMessage);
     }
 }
