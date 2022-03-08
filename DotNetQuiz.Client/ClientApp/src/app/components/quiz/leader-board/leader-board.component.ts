262f81a09b96733abd5faed74333ddda49fd5dec
Malukov Nikita <nikitaminsk833@gmail.com>
2022-03-08 16:15:21 +0300

    Add quiz leader-board component

---
+++ DotNetQuiz.Client/ClientApp/src/app/components/quiz/leader-board/leader-board.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { takeUntil, tap } from 'rxjs/operators';
+import { QuizPlayer } from 'src/app/models/quiz-player.model';
+import { QuizService } from 'src/app/services/quiz.service';
+import { DestroyableComponent } from 'src/app/utils/destroyable-component/destroyable.component';
+
+@Component({
+  selector: 'leader-board',
+  templateUrl: 'leader-board.component.html',
+  styleUrls: ['leader-board.component.scss'],
+})
+export class LeaderBoardComponent
+  extends DestroyableComponent
+  implements OnInit
+{
+  public quizPlayers: QuizPlayer[] = [];
+
+  @Input() sessionId!: string;
+  constructor(private readonly quizService: QuizService) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  private loadData() {
+    this.quizService
+      .getSessionPlayers(this.sessionId)
+      .pipe(
+        takeUntil(this.onDestroy$),
+        tap(
+          (quizPlayers) => (this.quizPlayers = this.quizPlayers = quizPlayers)
+        )
+      )
+      .subscribe();
+  }
+}
