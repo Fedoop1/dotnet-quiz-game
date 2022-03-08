eeff39ec60ff9d97558a06fc9a930693ee8da0b1
Malukov Nikita <nikitaminsk833@gmail.com>
2022-03-08 16:15:01 +0300

    Add quiz host component

---
+++ DotNetQuiz.Client/ClientApp/src/app/components/quiz/host/quiz-host.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quiz-host',
+  templateUrl: 'quiz-host.component.html',
+  styleUrls: ['quiz-host.component.scss'],
+})
+export class QuizHostComponent {}
