ac85f0c0540f38559deaa0b29806cda21fb4f0f5
Malukov Nikita <nikitaminsk833@gmail.com>
2022-03-13 23:00:49 +0300

    Update CORS policy

--- DotNetQuiz.WebApi/Program.cs
+++ DotNetQuiz.WebApi/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(config =>
     {
         config.AllowAnyHeader();
         config.AllowAnyMethod();
-        config.WithOrigins("http://localhost:4200", "http://localhost:6000");
+        config.WithOrigins("http://localhost:4200", "http://localhost:6000", "https://localhost:6001");
         config.AllowCredentials();
     });
 });
