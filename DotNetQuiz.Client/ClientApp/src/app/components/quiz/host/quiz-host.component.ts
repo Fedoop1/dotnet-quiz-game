06a7b237402e0289226e32f7621ce73f0075e06a
Malukov Nikita <nikitaminsk833@gmail.com>
2022-03-19 20:53:19 +0300

    Update quiz host component

--- DotNetQuiz.Client/ClientApp/src/app/components/quiz/host/quiz-host.component.ts
+++ DotNetQuiz.Client/ClientApp/src/app/components/quiz/host/quiz-host.component.ts
@@ -1,10 +1,11 @@
 import { registerLocaleData } from '@angular/common';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { switchMap, takeUntil, tap } from 'rxjs/operators';
 import { SessionState } from 'src/app/models/enums/round-state.enum.model';
 import { QuizData } from 'src/app/models/quiz-data.model';
 import { QuizPlayer } from 'src/app/models/quiz-player.model';
+import { Question } from 'src/app/models/quiz-question.model';
 import { QuizRound } from 'src/app/models/quiz-round.model';
 import { QuizService } from 'src/app/services/quiz.service';
 import { DestroyableComponent } from 'src/app/utils/destroyable-component/destroyable.component';
@@ -15,16 +16,25 @@ import { DateFormat } from './models/date-format.enum';
   selector: 'quiz-host',
   templateUrl: 'quiz-host.component.html',
   styleUrls: ['quiz-host.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-// TODO: Add timeout which track question time
 export class QuizHostComponent extends DestroyableComponent implements OnInit {
   public quizData!: QuizData;
   public currentRound!: QuizRound;
+  public quizQuestions!: Question[];
 
   public DateFormat = DateFormat;
   public isFirstRound = true;
 
-  @ViewChild('timer') timer!: RoundTimerComponent;
+  public get isRoundButtonsDisabled(): boolean {
+    return (
+      !this.quizQuestions ||
+      !this.quizQuestions.length ||
+      (this.quizQuestions && this.quizQuestions?.length === 1)
+    );
+  }
+
+  @ViewChild(RoundTimerComponent) timer!: RoundTimerComponent;
 
   constructor(
     private readonly quizService: QuizService,
@@ -36,16 +46,30 @@ export class QuizHostComponent extends DestroyableComponent implements OnInit {
   ngOnInit(): void {
     this.quizData = history.state.quiz;
 
-    // if (!this.quizData) this.router.navigate(['']);
+    if (!this.quizData) this.router.navigate(['']);
 
     this.loadData();
   }
 
   private loadData() {
-    this.loadRound().subscribe();
+    this.loadRound()
+      .pipe(
+        switchMap(() =>
+          this.quizService.getQuizQuestions(this.quizData.sessionId)
+        ),
+        tap((quizQuestions) => (this.quizQuestions = quizQuestions))
+      )
+      .subscribe();
   }
 
   public nextRound() {
+    this.quizQuestions.splice(
+      this.quizQuestions.findIndex(
+        (question) => question.questionId === this.currentRound.questionId
+      ),
+      1
+    );
+
     this.quizService
       .nextRound(this.quizData.sessionId)
       .pipe(
@@ -60,7 +84,17 @@ export class QuizHostComponent extends DestroyableComponent implements OnInit {
       .startRound(this.quizData.sessionId)
       .pipe(
         takeUntil(this.onDestroy$),
-        tap(() => this.timer.startTimer())
+        tap((roundTime) => {
+          const startAt = new Date(roundTime.startAt);
+          const endAt = new Date(roundTime.endAt);
+
+          this.currentRound.startAt = startAt;
+          this.currentRound.endAt = endAt;
+
+          setTimeout(() => {
+            this.timer.startTimer();
+          });
+        })
       )
       .subscribe();
   }
@@ -83,6 +117,11 @@ export class QuizHostComponent extends DestroyableComponent implements OnInit {
         tap(() => this.timer.stopTimer())
       )
       .subscribe();
+  }
+
+  public closeSession() {
+    this.quizService.disconnectFromHub();
+    this.router.navigate(['']);
   }
 
   private loadRound() {
