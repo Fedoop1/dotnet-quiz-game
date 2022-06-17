4b6b1e75b48c171adcee9f309795fdb7b0277a9b
Malukov Nikita <nikitaminsk833@gmail.com>
2022-06-17 03:12:48 +0300

    Add question validation to quiz configuration service

--- DotNetQuiz.Client/ClientApp/src/app/services/quiz-configuration.service.ts
+++ DotNetQuiz.Client/ClientApp/src/app/services/quiz-configuration.service.ts
@@ -3,6 +3,9 @@ import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { QuestionPack } from '../models/quiz-question-pack.model';
 import { QuizConfiguration } from '../models/quiz-configuration.model';
+import * as _ from 'lodash';
+import { Question } from '../models/quiz-question.model';
+import { QuestionType } from '../models/enums/question-type.enum';
 
 @Injectable()
 export class QuizConfigurationService {
@@ -29,17 +32,30 @@ export class QuizConfigurationService {
     );
   }
 
-  private validateQuestionPack(questionPack?: QuestionPack): boolean {
-    return (
-      (questionPack?.questions &&
-        questionPack.questions.every(
-          (question) =>
-            question.questionId &&
-            question.questionReward >= 0 &&
-            question.content &&
-            question.answer
-        )) ||
-      false
-    );
+  public validateQuestionPack(questionPack?: QuestionPack): boolean {
+    return (questionPack?.questions &&
+      !!questionPack?.questions.length &&
+      questionPack.questions.every((question) =>
+        this.validateQuestion(question)
+      ))!;
+  }
+
+  // TODO: Refactor this hard coded validation
+  private validateQuestion(question: Question): boolean {
+    if (!question) return false;
+
+    if (question.questionReward < 0) return false;
+    if (question.questionType === undefined) return false;
+    if (
+      question.questionType === QuestionType.Select &&
+      (!question.options || !question.options.length)
+    )
+      return false;
+
+    if (!question.content.questionBlob && !question.content.questionText)
+      return false;
+    if (!question.answer.answerContent) return false;
+
+    return true;
   }
 }
